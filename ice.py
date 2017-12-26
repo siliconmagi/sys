63b7b375804c0d9f056e07ec5a1c013a8fc4c414
siliconmagi <siliconmagi@yandex.com>
2017-12-25 22:54:48 -0800

    2017-12-25 22:54:41

--- ice.py
+++ ice.py
@@ -1,4 +1,5 @@
 import csv
+import subprocess
 from pexpect import pxssh
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
@@ -15,31 +16,42 @@ with open('secret.csv', 'r') as f:
         ipList.append({'name': row[0], 'ip': row[1],
                        'user': row[2], 'pwd': row[3]})
 
+# gen keywords from ipList
 keywords = [x['name'] for x in ipList]
-print(keywords.append('exit'))
+keywords.append('exit')
 
 
 def login(ip, user, pwd):
-    s = pxssh.pxssh()
-    s.SSH_OPTS = "-o 'RSAAuthentication=no' -o 'PubKeyAuthentication=no'"
-    s.login(ip, user, pwd)
-    s.sendline('whoami;uname -a')
-    before = s.before.decode('unicode_escape')
-    if 'psp' in before:
-        s.sendline('sudo su -')
-        s.expect('assword.*: ')
-        s.sendline(pwd)
-        s.expect('root.*')
-        s.sendline('whoami')
-        print(before)
-        s.interact()
-    elif 'root' in before:
-        print(before)
-        s.interact()
+    if user == 'administrator' or user == 'sysop':
+        option = 'rdesktop -g 1280x960'
+        bashChain = "{} -u {} -p '{}' {}".format(option, user, pwd, ip)
+        print(bashChain)
+        # subprocess for cmds
+        process = subprocess.Popen(
+            '/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, err = process.communicate(bashChain.encode('utf-8'))
+        print(out.decode('utf-8'))
     else:
-        print('Unknown Login')
-        print(s.before.decode('unicode_escape'))
-        s.interact()
+        s = pxssh.pxssh()
+        s.SSH_OPTS = "-o 'RSAAuthentication=no' -o 'PubKeyAuthentication=no'"
+        s.login(ip, user, pwd)
+        s.sendline('whoami;uname -a')
+        before = s.before.decode('unicode_escape')
+        if 'psp' in before:
+            s.sendline('sudo su -')
+            s.expect('assword.*: ')
+            s.sendline(pwd)
+            s.expect('root.*')
+            s.sendline('whoami')
+            print(before)
+            s.interact()
+        elif 'root' in before:
+            print(before)
+            s.interact()
+        else:
+            print('Unknown Login')
+            print(before)
+            s.interact()
 
 
 class iceCompleter(Completer):
@@ -51,15 +63,17 @@ class iceCompleter(Completer):
 
 
 while 1:
+    # create prompt
     user_input = prompt(u'iceREPL>',
                         history=FileHistory('history.txt'),
                         auto_suggest=AutoSuggestFromHistory(),
                         completer=iceCompleter(),
                         vi_mode=True,
                         )
+    # check input against name in ipList
     if user_input in [x['name'] for x in ipList]:
-        user = [x['user'] for x in ipList if x['name'] == user_input][0]
         ip = [x['ip'] for x in ipList if x['name'] == user_input][0]
+        user = [x['user'] for x in ipList if x['name'] == user_input][0]
         pwd = [x['pwd'] for x in ipList if x['name'] == user_input][0]
         login(ip, user, pwd)
     elif user_input == 'exit':
